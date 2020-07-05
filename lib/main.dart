1bda4e0d2ae5079f499a6f8d2c0bb63aea7ef2fd
Made Indra <made.indra@protonmail.com>
2020-07-05 19:23:49 +0700

    add scrollView

--- lib/main.dart
+++ lib/main.dart
@@ -18,19 +18,21 @@ class MyHomepage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: Text('My Financial Manager')),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              width: double.infinity,
-              child: Card(
-                child: Center(child: Text("Overview")),
-                elevation: 5,
+        body: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                width: double.infinity,
+                child: Card(
+                  child: Center(child: Text("Overview")),
+                  elevation: 5,
+                ),
               ),
-            ),
-            TransactionAction(),
-          ],
+              TransactionAction(),
+            ],
+          ),
         ));
   }
 }
